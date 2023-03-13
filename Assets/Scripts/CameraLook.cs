c856922dce9b09df007ef155ff93cb6ef0772482
goon <nathan.gooneratne@gmail.com>
2023-03-13 13:27:46 -0400

    camera lock

--- Assets/Scripts/CameraLook.cs
+++ Assets/Scripts/CameraLook.cs
@@ -7,12 +7,16 @@ public class CameraLook : MonoBehaviour
     Transform playerBody;
 
     public float mouseSensitivity = 10f;
+    public float leeway = 35f;
+    float original_angle;
 
     float pitch = 0;
     // Start is called before the first frame update
     void Start()
     {
         playerBody = transform.parent.transform;
+
+        original_angle = playerBody.eulerAngles.y;
 
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
@@ -23,9 +27,13 @@ public class CameraLook : MonoBehaviour
     {
         float moveX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float moveY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        //yaw
+        Vector3 e = playerBody.eulerAngles;
+        e.y = Mathf.Clamp(e.y + moveX, original_angle - leeway, leeway + original_angle);
+        playerBody.eulerAngles = e;
         
-            //yaw
-        playerBody.Rotate(Vector3.up * moveX);
+        
 
             //pitch
         pitch -= moveY;
